7b5fd9363492f736e8ac60eaec1e897240504154
Ake Tangkananond <iamake@gmail.com>
2017-07-09 18:29:50 +0700

    Update the App.js to use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 
+import './assets/css/styles.scss';
 import styles from './App.local.scss';
 
 const cx = classNames.bind(styles);
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Hello</h1>
       <span className={cx('iconSmile', 'icon-bordered')}></span>
     </div>
